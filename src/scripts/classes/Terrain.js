897c8d00cd6dfee69fdb99eddc12311ec0663153
Maja Persson <persson.maja@gmail.com>
2018-10-22 11:08:04 +0200

    Changed rotation to radians, it works now!!

--- src/scripts/classes/Terrain.js
+++ src/scripts/classes/Terrain.js
@@ -1,41 +1,63 @@
 import {
   BufferAttribute,
   Mesh,
-  MeshBasicMaterial,
+  MeshPhongMaterial,
   PlaneBufferGeometry
 } from "three";
 import SimplexNoise from "simplex-noise";
 
-const SIZE = 5;
-const SEGMENTS = 20;
-const ROTATION = Math.PI / -3;
+import { toRadians } from "../utils";
 
-const SMOOTHING = 0.4;
+const SIZE = 10;
+const SEGMENTS = 40;
+
+const SMOOTHING = 4;
 const HEIGHT = 0.5;
 
 const simplex = new SimplexNoise();
 
 export default class Terrain {
   constructor() {
-    this.geometry = new PlaneBufferGeometry(SIZE, SIZE, SEGMENTS, SEGMENTS);
-    this.setZ();
-    this.material = new MeshBasicMaterial({ wireframe: true });
+    this.size = SIZE;
+    this.segments = SEGMENTS;
+
+    this.geometry = new PlaneBufferGeometry(
+      this.size,
+      this.size,
+      this.segments,
+      this.segments
+    );
+    this.setHeight();
+    this.material = new MeshPhongMaterial({
+      color: 0x00ff00,
+      shininess: 0
+    });
     this.mesh = new Mesh(this.geometry, this.material);
-    this.mesh.rotation.z = ROTATION;
-    this.mesh.rotation.x = ROTATION;
+    this.mesh.rotation.x = toRadians(-90);
   }
 
-  setZ() {
+  setHeight() {
     const vertices = this.geometry.getAttribute("position").array;
 
     for (let i = 0; i < vertices.length; i++) {
       if ((i + 1) % 3 === 0) {
         const x = vertices[i - 2];
         const y = vertices[i - 1];
-        vertices[i] = simplex.noise2D(x * SMOOTHING, y * SMOOTHING) * HEIGHT;
+        vertices[i] = simplex.noise2D(x / SMOOTHING, y / SMOOTHING) * HEIGHT;
       }
     }
 
     this.geometry.addAttribute("position", new BufferAttribute(vertices, 3));
+    this.geometry.computeVertexNormals();
+  }
+
+  getHeightAt(x, y) {
+    const vertices = this.geometry.getAttribute("position").array;
+
+    const z = vertices.filter(
+      (vertex, i) =>
+        (i + 1) % 3 === 0 && vertices[i - 2] === x && vertices[i - 1] === y
+    );
+    return z[0];
   }
 }
